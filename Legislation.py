146710148dcbdfba70f56059e16013a2a1ba30d9
mband0 <31228859+mband0@users.noreply.github.com>
2020-06-03 03:56:31 -0400

    Update Legislation.py

--- Legislation.py
+++ Legislation.py
@@ -4,6 +4,14 @@ class Legislation:
     _description = None
     _date = None
     _text = None
+    _evaluations = {}
 
-class Bill:
+    def __init__(self, name, ID, desc, date, text=None):
+        self._name = name
+        self._id = ID
+        self._description = desc
+        self._date = date
+        self._text = text
+
+class Bill(Legislation):
     _house = None
